36e9a07b30e7cb37203704bee4db3f49123d4a32
Debajyoti Dutta <debajyotidutta.50@gmail.com>
2023-09-03 22:17:27 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Tooltip from './Components/Tooltip/Tooltip';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='tooltip-components'>
+        <h1>This is tooltip</h1>
+        <Tooltip position="top" text="This is a top tooltip">
+          <button>Top Hover</button>
+        </Tooltip>
+
+        <Tooltip position="bottom" text="This is a bottom tooltip">
+          <button>Bottom Hover</button>
+        </Tooltip>
+
+        <Tooltip position="left" text="This is a left tooltip">
+          <button>Left Hover</button>
+        </Tooltip>
+
+        <Tooltip position="right" text="This is a right tooltip">
+          <button>Right Hover</button>
+        </Tooltip>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Tooltip/Tooltip.js
@@ -0,0 +1,17 @@
+import styles from './Tooltip.module.css'; // Import your CSS file for styling
+
+const Tooltip = ({ position, text, children }) => {
+  // Create a dynamic positioning class based on the "position" prop
+  const positionClass = styles[position];
+
+  return (
+    <div className={styles.tooltip}>
+      {children} {/* The button to which the tooltip is attached */}
+      <span className={`${styles.tooltiptext} ${positionClass}`}>
+        {text} {/* The text content of the tooltip */}
+      </span>
+    </div>
+  );
+};
+
+export default Tooltip;
